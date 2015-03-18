7396b95cc0ba78f0adb77cf390c5b664f202dee4
wangdingkang <1257332448@qq.com>
2015-03-18 15:54:43 -0400

    version 3

---
+++ version 3/src/graphic/Main.java
@@ -0,0 +1,137 @@
+package graphic;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import model.MySymbol;
+import utils.IOUtils;
+
+public class Main {
+
+	private static PaintingPanel paintPanel = null;
+	private static JTextArea latexArea = null;
+	private static JTextArea messageArea = null;
+	private static JTextField numField = null;
+	// get dataset
+	private static List<MySymbol> symbols = new ArrayList<MySymbol>();
+	
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager
+					.getCrossPlatformLookAndFeelClassName());
+			JFrame myFrame = new JFrame("Recognizer");
+			myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			myFrame.setLayout(null);
+			myFrame.setSize(1000, 700);
+			myFrame.setResizable(false);
+			//Container c = myFrame.getContentPane();
+
+			JButton clearButton = new JButton("clear");
+			clearButton.setBounds(5, 5, 100, 30);
+			myFrame.add(clearButton);
+
+			clearButton.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					// TODO Auto-generated method stub
+					paintPanel.clear();
+				}
+
+			});
+
+			JButton submitButton = new JButton("submit");
+			submitButton.setBounds(110, 5, 100, 30);
+			myFrame.add(submitButton);
+
+			submitButton.addActionListener(new ActionListener(){
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					// TODO Auto-generated method stub
+					//paintPanel.saveImage();
+					// paintPanel.recognize();
+				}
+				
+			});
+			
+			JButton addButton = new JButton("addData");
+			addButton.setBounds(215, 5, 100, 30);
+			myFrame.add(addButton);
+			
+			addButton.addActionListener(new ActionListener(){
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					// TODO Auto-generated method stub
+					// int v = Integer.valueOf(numField.getText());
+					// symbols.addAll(paintPanel.generateData(v));
+				}
+				
+			});
+			
+			JButton commitButton = new JButton("commit");
+			commitButton.setBounds(320, 5, 100, 30);
+			myFrame.add(commitButton);
+			
+			commitButton.addActionListener(new ActionListener(){
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					// TODO Auto-generated method stub
+					// int numStroke = symbols.get(0).getStrokes().size();
+					// int v = Integer.valueOf(numField.getText());
+					// IOUtils.objectToJsonFile(v, numStroke, symbols);
+					// symbols.clear();
+					paintPanel.recognize();
+				}
+				
+			});
+			
+			numField = new JTextField();
+			numField.setBounds(425, 5, 200, 30);
+			myFrame.add(numField);
+			
+			paintPanel = new PaintingPanel();
+			paintPanel.setBackground(Color.WHITE);
+			paintPanel.setBounds(5, 40, 985, 300);
+			myFrame.add(paintPanel);
+
+			JLabel label1 = new JLabel("Message");
+			label1.setBounds(5, 350, 80, 30);
+			myFrame.add(label1);
+			
+			JLabel label2 = new JLabel("Latex");
+			label2.setBounds(510, 350, 80, 30);
+			myFrame.add(label2);
+
+			messageArea = new JTextArea();
+			messageArea.setBounds(5, 390, 480, 200);
+			messageArea.setEditable(false);
+			messageArea.setText("TEST");
+			myFrame.add(messageArea);
+
+			latexArea = new JTextArea();
+			latexArea.setBounds(510, 390, 480, 200);
+			latexArea.setEditable(false);
+			myFrame.add(latexArea);
+
+			myFrame.setVisible(true);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ version 3/src/graphic/MyRect.java
@@ -0,0 +1,140 @@
+package graphic;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyRect {
+
+	private long ID;
+
+	private static final Color activeColor = Color.green;
+	private static final Color inactiveColor = Color.red;
+	
+	public List<MyLine> lineList = null;
+
+	public int lx, ly, rx, ry;
+	public int width, height;
+
+	public boolean isActive;
+	
+	private int value = -1;
+
+	private String latex = null; // symbol show on the top-right corner.
+	
+	public MyRect(long id)
+	{
+		ID = id;
+		latex = new String("");
+		lineList = new ArrayList<MyLine>();
+		lx = ly = Integer.MAX_VALUE;
+		rx = ry = Integer.MIN_VALUE;
+		this.isActive = true;
+	}
+	
+	public MyRect(long id, MyLine line, boolean ia) {
+		ID = id;
+		latex = new String("");
+		lineList = new ArrayList<MyLine>();
+		lineList.add(line);
+
+		lx = line.lt.x;
+		ly = line.lt.y;
+
+		rx = line.rb.x;
+		ry = line.rb.y;
+
+		width = rx - lx;
+		height = ry - ly;
+
+		this.isActive = ia;
+	}
+
+	public boolean isMe(long aid)
+	{
+		return (this.ID == aid);
+	}
+	
+	public void join(MyRect another) {
+		for (MyLine line : another.lineList) {
+			this.lineList.add(line);
+		}
+		lx = Math.min(this.lx, another.lx);
+		ly = Math.min(this.ly, another.ly);
+		rx = Math.max(this.rx, another.rx);
+		ry = Math.max(this.ry, another.ry);
+		width = rx - lx;
+		height = ry - ly;
+	}
+	
+	public void join(MyLine mline)
+	{
+		this.lineList.add(mline);
+		lx = Math.min(this.lx, mline.lt.x);
+		ly = Math.min(this.ly, mline.lt.y);
+		rx = Math.max(this.rx, mline.rb.x);
+		ry = Math.max(this.ry, mline.rb.y);
+		width = rx - lx;
+		height = ry - ly;
+	}
+
+	public boolean isTouching(MyRect another) {
+		int cx1 = this.lx + this.rx;
+		int cy1 = this.ly + this.ry;
+		int cx2 = another.lx + another.rx;
+		int cy2 = another.ly + another.ry;
+		int dx = this.width + another.width + 20;
+		int dy = this.height + another.height + 20;
+		return (Math.abs(cx1 - cx2) < dx && Math.abs(cy1 - cy2) < dy);
+	}
+
+	public long getID() {
+		return ID;
+	}
+	
+	public void setLatex(String res)
+	{
+		this.latex = res;
+	}
+	
+	public String getLatex()
+	{
+		return this.latex;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+	public void drawLines(Graphics2D g2d) {
+		for (MyLine line : this.lineList) {
+			line.draw(g2d);
+		}
+	}
+
+	public void drawRect(Graphics2D g2d) {
+		if (this.isActive) {
+			g2d.setColor(activeColor);
+			g2d.drawRect(lx-10, ly-10, width+20, height+20);
+		}
+		else
+		{
+			g2d.setColor(inactiveColor);
+			g2d.drawRect(lx-1, ly-1, width+2, height+2);
+		}
+		if(!latex.equals("") && latex!=null)
+		{
+			g2d.setColor(Color.BLACK);
+			g2d.drawString(latex, rx, ly);
+		}
+	}
+
+	// public ... getData();
+
+}
